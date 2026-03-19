e84b97b19697534abb221aa5f1ad8b7bebea6d37
Willem Ave <willem.ave@gmail.com>
2026-03-18 20:26:24 -0700

    Relax formula test matcher

--- Formula/reviewbuddy.rb
+++ Formula/reviewbuddy.rb
@@ -34,7 +34,7 @@ class Reviewbuddy < Formula
   end
 
   test do
-    assert_match "uv tool run --from", (bin/"reviewbuddy").read
+    assert_match "tool run --from", (bin/"reviewbuddy").read
     assert_path_exists pkgshare/"skills/reviewbuddy-tap-maintainer/SKILL.md"
   end
 end
